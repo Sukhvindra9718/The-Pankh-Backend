a0552eaff674b15e6be6951861fdb76c131e5203
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-08-17 17:51:11 +0530

    config change

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     port: 5432,
   },
   jwtSecret: "thisissecretconversationroomdontinterfere",
-  PORT: 5000,
+  PORT: 3000,
   NODE_ENV: "production",
   SMTP_HOST: "smtp.gmail.com",
   SMTP_PORT: 465,
