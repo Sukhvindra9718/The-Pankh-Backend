3982bd0045da46a3bc027059528a043848862069
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-08-27 01:42:58 +0530

    test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ readdirSync("./routes").map((route) => app.use("/api", require("./routes/" + rou
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 // app.use(cors());
-app.use(cors({ origin: "http://thepankh.info/" }));
+app.use(cors({ origin: "https://thepankh.info" }));
 app.use(express.json({ limit: "50mb" }));
 
 // Import Routes
